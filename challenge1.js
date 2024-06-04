9b210838ef13d34e2c96ea7c1ba87bb4bbca282b
mbabaziemelyne <uwimbeme@gmail.com>
2024-06-04 14:39:28 +0200

    chllng1 modified

--- challenge1.js
+++ challenge1.js
@@ -1,9 +1,23 @@
 // coding challenge 1: finding the subarray that sums up to the target
 
-function findSubbary(array, target){
-    return array.filter((num, index) => num[index] + num[index+1] === target)
+function findSubbarray(arr, target) {
+    for (let startindex = 0; startindex < arr.length; startindex++) {
+        let currentSum = 0;
+        
+        for (let end = startindex; end < arr.length; end++) {
+            currentSum += arr[end];
+            
+            if (currentSum === target) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
 }
 
-let arr1 = [4, 2, 7, 1, 9, 5];
-
-console.log(findSubbary(arr1, 6));
+const arr = [4, 2, 7, 1, 9, 5];
+const target = 17;
+const target2 = 27;
+console.log(findSubbarray(arr, target));
+console.log(findSubbarray(arr, target2))
